4da68815226ec19c54083c50507e4fbab8ae27f4
sagarsuthar2362 <1sagarsuthar2362@gmail.com>
2025-06-13 14:58:26 +0530

    port changes

--- server/index.js
+++ server/index.js
@@ -62,6 +62,6 @@ app.put("/update/:id", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("server running on port 5000");
+app.listen(3000, () => {
+  console.log("server running on port 3000");
 });
